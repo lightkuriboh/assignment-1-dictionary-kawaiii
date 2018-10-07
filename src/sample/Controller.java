2cd3d9973dabd47987fe5344a887e7d274b63c80
lightkuriboh <kuribohwithwing@gmail.com>
2018-10-07 13:23:19 +0700

    Add Event Hint Words

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,26 +2,61 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
+import javafx.scene.input.InputEvent;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.input.InputMethodTextRun;
 import sample.DocumentTranslate.DocumentTranslate;
 import sample.WordTranslate.WordTranslate;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        this.searchBar.textProperty().addListener((obs, oldText, newText) -> {
+            this.textChangeInput(newText);
+        });
+    }
+
     @FXML
     private TextArea searchDoc;
 
     @FXML
-    private Label translatedText;
+    private TextArea translatedText;
 
     private DocumentTranslate myDocumentTranslate = new DocumentTranslate();
-    private WordTranslate myWordTranslate = new WordTranslate();
 
     public void submitDocument(ActionEvent event) {
         String document = this.searchDoc.getText();
         String translated = this.myDocumentTranslate.translateIt("en", "vi", document);
         this.translatedText.textProperty().setValue(translated);
     }
+
+
+    @FXML
+    private ListView Hints;
+
+    @FXML
+    private TextField searchBar;
+
+    @FXML
+    private Label wordDetails;
+
+    private WordTranslate myWordTranslate = new WordTranslate();
+
+    public void submitSearchWord(ActionEvent event) {
+        String word = this.searchBar.getText();
+        System.out.println(word);
+        this.wordDetails.textProperty().setValue(word);
+    }
+
+    public void textChangeInput(String word) {
+
+    }
+
 }
